da6a66b8e2729c29699466e5b01b9b7d1ef2b58e
pixelperfectmeetanshi-max <pixelperfectmeetanshi@gmail.com>
2025-11-21 18:15:25 +0530

    Figma Plugins

--- AI Background Remover/code.ts
+++ AI Background Remover/code.ts
@@ -10,7 +10,7 @@
 figma.showUI(__html__);
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
+// callback. The callback will be passed the "pluginMessage " property of the
 // posted message.
 figma.ui.onmessage =  (msg: {type: string, count: number}) => {
   // One way of distinguishing between different types of messages sent from
